7efb8b6cbf981a73c0b0dd62805e16f6b11c4c28
shamsiddinhakimov <shamsiddinhakimov@gmail.com>
2024-02-12 19:47:32 +0500

    this is my new code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from './Button'
+
+
+
+function App() {
+  
+  return (
+   <Button value="2"></Button>
+  )
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,26 @@
+import React, { useMemo } from 'react';
+
+// export default function ExampleComponent({ value }) {
+//   const expensiveCalculation = useMemo(() => {
+   
+//     return value * 1;
+//   }, [value]);
+
+//   return <div>{expensiveCalculation}</div>;
+// }
+// export default function ExampleComponent({ value }) {
+//     const expensiveCalculation = useMemo(() => {
+     
+//       return value * 10;
+//     }, [value]);
+  
+//     return <div>{expensiveCalculation}</div>;
+//   }
+export default function ExampleComponent({ value }) {
+    const expensiveCalculation = useMemo(() => {
+     
+      return value * 20;
+    }, [value]);
+  
+    return <div>{expensiveCalculation}</div>;
+  }
