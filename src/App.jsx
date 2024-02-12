5e5e61f65f494657e49f1da9cc12bfb6dc1ca8e2
shamsiddinhakimov <shamsiddinhakimov@gmail.com>
2024-02-12 19:52:11 +0500

    sasasa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Button from './Button'
-
+import React, { useCallback } from 'react';
 
 
 function App() {
+  // const handleClick = useCallback(() => {
+      
+  //   onClick();
+  // }, [onClick]);
+
+
+
+  return (<>
+  <Button value="2"></Button>
+   <button onClick={handleClick}>Bosing</button>
   
-  return (
-   <Button value="2"></Button>
+  </>
+   
   )
 }
 
--- src/Button.jsx
+++ src/Button.jsx
@@ -24,3 +24,5 @@ export default function ExampleComponent({ value }) {
   
     return <div>{expensiveCalculation}</div>;
   }
+
+
